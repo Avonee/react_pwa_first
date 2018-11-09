5e489c435e889caad2445cf9922566827d3af16f
avon <avoncheng@ampostech.com>
2018-11-09 10:29:27 +0800

    add skype and evernote link test

--- src/App.js
+++ src/App.js
@@ -32,13 +32,24 @@ class App extends Component {
 
           <a
             className="App-link"
-            href="https://www.skype.com"
+            href= "skype://raquelmota1977?chat"//"https://www.skype.com/zh-Hant/get-skype/"//"https://www.skype.com"
             target="_blank"
             rel="noopener noreferrer"
           >
           <img src="https://www.vectorlogo.zone/logos/skype/skype-tile.svg" class="styled-icon"></img>
             Skype
           </a>
+
+          <a
+            className="App-link"
+            href= "https://www.evernote.com/notelink/Login.action?targetUrl=%2Fshard%2Fs3%2Fnl%2F237001%2F232d6d1e-11b5-40f3-aa53-f278f070f5a7"//"https://www.evernote.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          <img src="https://images.fastcompany.net/image/upload/w_1280,f_jpg,q_auto,fl_lossy/wp-cms/uploads/2018/08/p-2-evernote.gif" class="styled-icon"></img>
+            Evernote
+          </a>
+
 
         </header>
       </div>
