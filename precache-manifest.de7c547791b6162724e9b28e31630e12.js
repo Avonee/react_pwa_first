d49f939fe6c4d234b64da98b609b897f7efce820
avon <avoncheng@ampostech.com>
2018-11-13 16:49:41 +0800

    Rebuild website

---
+++ precache-manifest.de7c547791b6162724e9b28e31630e12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_pwa_first/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c11eb1ba2fd7b31c694a",
+    "url": "/react_pwa_first/static/js/runtime~main.c11eb1ba.js"
+  },
+  {
+    "revision": "a21e83cfd82ed33cba6f",
+    "url": "/react_pwa_first/static/js/main.a21e83cf.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/react_pwa_first/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "a21e83cfd82ed33cba6f",
+    "url": "/react_pwa_first/static/css/main.a3566d2f.chunk.css"
+  },
+  {
+    "revision": "cffc7a01811faf30f94fed66a67ac703",
+    "url": "/react_pwa_first/index.html"
+  }
+];
